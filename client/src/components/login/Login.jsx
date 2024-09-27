99c1a3b53065415a44ada75188ea788fa5d5f950
priyanshsingh <priyanshsingh1401@gmail.com>
2024-09-27 15:53:15 +0530

    starter login component added

---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './login.css';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div className="login-container">
+            <h1 className="login-heading">Login</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="login-input"
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="login-input"
+                    required
+                />
+                <button type="submit" className="login-button">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/components/plp/Plp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Plp() {
+  return (
+    <div>Plp</div>
+  )
+}
+
+export default Plp
